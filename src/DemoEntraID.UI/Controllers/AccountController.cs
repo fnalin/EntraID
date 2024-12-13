81a7c61369ccec3ee56200c01e10f164ba366e13
Fabiano Nalin <fnalin@msn.com>
2024-12-13 22:10:07 +0100

    add urls config

--- src/DemoEntraID.UI/Controllers/AccountController.cs
+++ src/DemoEntraID.UI/Controllers/AccountController.cs
@@ -9,6 +9,14 @@ namespace DemoEntraID.UI.Controllers;
 
 public class AccountController : Controller
 {
+    /*
+      - In the Authentication section, perform the following actions, and select Configure:
+       Redirect URIs text box	Enter https://localhost:5001/
+       Front-channel logout URL text box	Enter https://localhost:5001/signout-oidc
+       Back in the Platform configurations section, select Add URI, and then enter https://localhost:5001/signin-oidc.
+       
+       In the Implicit grant and hybrid flows section, select ID tokens (used for implicit and hybrid flows).
+     */
     public IActionResult Login(string returnUrl = "/")
     {
         return Challenge(new AuthenticationProperties { RedirectUri = returnUrl }, OpenIdConnectDefaults.AuthenticationScheme);
